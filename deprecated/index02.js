ce67f71ae746dd031450b0c27ab766ff53dfc6ee
davidnoah <david.t.noah@gmail.com>
2017-01-25 10:02:05 -0800

    update alexa skill

---
+++ deprecated/index02.js
@@ -0,0 +1,194 @@
+'use strict';
+const RapidAPI = require('rapidapi-connect');
+const rapid = new RapidAPI('demo', '12345678');
+
+/**
+ * This is the code behind the translate skill
+ */
+
+
+// --------------- Helpers that build all of the responses -----------------------
+
+function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
+    return {
+        outputSpeech: {
+            type: 'PlainText',
+            text: output,
+        },
+        card: {
+            type: 'Simple',
+            title: `SessionSpeechlet - ${title}`,
+            content: `SessionSpeechlet - ${output}`,
+        },
+        reprompt: {
+            outputSpeech: {
+                type: 'PlainText',
+                text: repromptText,
+            },
+        },
+        shouldEndSession,
+    };
+}
+
+function buildResponse(sessionAttributes, speechletResponse) {
+    return {
+        version: '1.0',
+        sessionAttributes,
+        response: speechletResponse,
+    };
+}
+
+
+// --------------- Functions that control the skill's behavior -----------------------
+
+function getWelcomeResponse(callback) {
+    // If we wanted to initialize the session to have some attributes we could add those here.
+    const sessionAttributes = {};
+    const cardTitle = 'Welcome';
+    const speechOutput = 'Welcome to the translate skill. Say something like translate butterfly to German';
+    // If the user either does not reply to the welcome message or says something that is not
+    // understood, they will be prompted again with this text.
+    const repromptText = 'Say something like translate butterfly to German';
+    const shouldEndSession = false;
+
+    callback(sessionAttributes,
+        buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+}
+
+function handleSessionEndRequest(callback) {
+    const cardTitle = 'Session Ended';
+    const speechOutput = 'Thank you for trying the Translate. Have a nice day!';
+    // Setting this to true ends the session and exits the skill.
+    const shouldEndSession = true;
+
+    callback({}, buildSpeechletResponse(cardTitle, speechOutput, null, shouldEndSession));
+}
+
+/**
+ * Intent handlers
+ */
+ 
+function translate(intent, session, callback) {
+    const GoogleAPIKey = 'AIzaSyCDogEcpeA84USVXMS471PDt3zsG-caYDM';
+
+    const langCodes = {
+        "German" : "de",
+        "Dutch" : "nl",
+        "English" : "en",
+        "French" : "fr",
+        "Italian" : "it",
+        "Polish" : "pl",
+        "Russian" : "ru",
+        "Spanish" : "es"
+    };
+    const source = intent.slots.Source.value;
+    const lang = intent.slots.Language.value;
+    const langCode = langCodes[lang];
+
+    //Call RapidAPI
+    rapid.call('GoogleTranslate', 'translateAutomatic', {
+        string : source,
+        apiKey: GoogleAPIKey,
+        targetLanguage: langCode
+    })
+        .on('error', (payload) => {
+            callback({},
+                buildSpeechletResponse('Translate Demo', `Sorry, translation not available`, null, false));
+        })
+        .on ('success', (payload) => {
+            callback({},
+                buildSpeechletResponse('Translate Demo', `${source} in ${lang} is ${payload}`, null, false));
+        });
+
+    //callback({}, buildSpeechletResponse('Translate Demo', `Translating ${source} to ${lang}`, null, false));
+}
+
+
+// --------------- Events -----------------------
+
+/**
+ * Called when the session starts.
+ */
+function onSessionStarted(sessionStartedRequest, session) {
+    console.log(`onSessionStarted requestId=${sessionStartedRequest.requestId}, sessionId=${session.sessionId}`);
+}
+
+/**
+ * Called when the user launches the skill without specifying what they want.
+ */
+function onLaunch(launchRequest, session, callback) {
+    console.log(`onLaunch requestId=${launchRequest.requestId}, sessionId=${session.sessionId}`);
+
+    // Dispatch to your skill's launch.
+    getWelcomeResponse(callback);
+}
+
+/**
+ * Called when the user specifies an intent for this skill.
+ */
+function onIntent(intentRequest, session, callback) {
+    console.log(`onIntent requestId=${intentRequest.requestId}, sessionId=${session.sessionId}`);
+
+    const intent = intentRequest.intent;
+    const intentName = intentRequest.intent.name;
+
+    // Dispatch to your skill's intent handlers
+    if (intentName === 'Translate') {
+        translate(intent, session, callback);
+    } else {
+        throw new Error('Invalid intent');
+    }
+}
+
+/**åå
+ * Called when the user ends the session.
+ * Is not called when the skill returns shouldEndSession=true.
+ */
+function onSessionEnded(sessionEndedRequest, session) {
+    console.log(`onSessionEnded requestId=${sessionEndedRequest.requestId}, sessionId=${session.sessionId}`);
+    // Add cleanup logic here
+}
+
+
+// --------------- Main handler -----------------------
+
+// Route the incoming request based on type (LaunchRequest, IntentRequest,
+// etc.) The JSON body of the request is provided in the event parameter.
+module.exports.handler = (event, context, callback) => {
+    try {
+        console.log(`event.session.application.applicationId=${event.session.application.applicationId}`);
+
+        /**
+         * Uncomment this if statement and populate with your skill's application ID to
+         * prevent someone else from configuring a skill that sends requests to this function.
+         */
+        /*
+        if (event.session.application.applicationId !== 'amzn1.echo-sdk-ams.app.[unique-value-here]') {
+             callback('Invalid Application ID');
+        }
+        */
+
+        if (event.session.new) {
+            onSessionStarted({ requestId: event.request.requestId }, event.session);
+        }
+
+        if (event.request.type === 'LaunchRequest') {
+            onLaunch(event.request,
+                event.session,
+                (sessionAttributes, speechletResponse) => {
+                    callback(null, buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === 'IntentRequest') {
+            onIntent(event.request,
+                event.session,
+                (sessionAttributes, speechletResponse) => {
+                    callback(null, buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === 'SessionEndedRequest') {
+            onSessionEnded(event.request, event.session);
+            callback();
+        }
+    } catch (err) {
+        callback(err);
+    }
+};
